ddc8c1c366da026f0d015c892b4b8f6c15d0a10a
Tak Tran <contact@tutaktran.com>
2011-10-06 11:38:08 +0100

    Fix up formatting and added comments for js file.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,27 +1,29 @@
 $(document).ready(function(){
-	var colors = ["#e0427b","#419fda","#adca3a", "#f7ae35", "#42b8be", "#a1579c"];                
-	var rand = Math.floor(Math.random()*colors.length);           
-	$('nav').css("background-color", colors[rand]);
-	$('a[href*="work/#"]').click(function() {
+  var colors = ["#e0427b","#419fda","#adca3a", "#f7ae35", "#42b8be", "#a1579c"];                
+  var rand = Math.floor(Math.random()*colors.length);           
+  $('nav').css("background-color", colors[rand]);
+  
+  // Fix offset for anchor text, to accomodate for fixed header
+  $('a[href*="work/#"]').click(function() {
 
-		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
-			&& location.hostname == this.hostname) {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+      && location.hostname == this.hostname) {
 
-			var $target = $(this.hash);
+      var $target = $(this.hash);
 
-			$target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
+      $target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
 
-			if ($target.length) {
+      if ($target.length) {
 
-				var targetOffset = $target.offset().top - 300;
+        var targetOffset = $target.offset().top - 300;
 
-				$('html,body').animate({scrollTop: targetOffset}, 10);
+        $('html,body').animate({scrollTop: targetOffset}, 10);
 
-				return false;
+        return false;
 
-			}
+      }
 
-		}
+    }
 
-	});
+  });
 });
