c0dac4849e360fbe1f98045432fc6d71ee7a6858
Tak Tran <contact@tutaktran.com>
2012-04-23 14:07:56 +0100

    [#15] Add temporary folder for v2 stylesheet/javascript files

---
+++ public/v2/javascripts/script.js
@@ -0,0 +1,58 @@
+$(document).ready(function() {
+  /*
+  * Slideshow using http://slidesjs.com/
+  * play = time for each slide
+  */
+  if ($(".slideshow").length > 0) {
+    $(".slideshow").slides({
+      play: 5500,
+    });
+  }
+
+  /*
+  * random colours
+  */
+  function randOrd() {
+    return (Math.round(Math.random())-0.5); 
+  }
+  
+  var colors = [ 'pink', 'green', 'blue', 'orange', 'aqua', 'purple','pink', 'green', 'blue', 'orange', 'aqua', 'purple'];
+  
+  colors.sort( randOrd );
+  $('.background-random').each(function(i, val) {
+    $(this).addClass(colors[i]+"-background");
+  });
+  
+  colors.sort( randOrd );
+  $('.case-study').each(function(i, val) {
+    $("h2, h3",this).addClass(colors[i]);
+    $("hr, .img",this).addClass(colors[i]+"-background");
+  });
+  
+  colors.sort( randOrd );
+  $('.blog-post').each(function(i, val) {
+    $("h2, h3, a",this).addClass(colors[i]);
+    $(".img, .comments",this).addClass(colors[i]+"-background");
+    $(".blog-content",this).addClass(colors[i]+"-border");
+  });
+  
+  colors.sort( randOrd );
+  $('.spotlight').each(function(i, val) {
+    $("h2, h3, a",this).addClass(colors[i]);
+    $(".img",this).addClass(colors[i]+"-background");
+  });
+  
+  colors.sort( randOrd );
+  $('.person').each(function(i, val) {
+    $("h4",this).addClass(colors[i]);
+    $(".img",this).addClass(colors[i]+"-background");
+  });
+
+  colors.sort( randOrd );
+  $('.person').each(function(i, val) {
+    $("h4",this).addClass(colors[12]);
+    $(".img",this).addClass(colors[12]+"-background");
+  });
+
+});
+
