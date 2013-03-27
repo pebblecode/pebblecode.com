3b6df7e9fb6efb17c77a35a80863d7ec3bf42e9b
Tak Tran <contact@tutaktran.com>
2013-03-27 13:40:33 +1100

    Move pebbles good code js to its own file.

---
+++ public/javascripts/app/pebbles-good-code.js
@@ -0,0 +1,22 @@
+require([
+  "jquery",
+  "jquery.validator"
+], function($, jValidator) {
+  /*
+   * Pebble's good code
+   */
+  if ($.validator) {
+    var countWords = function(s) {
+      s = s.replace(/(^\s*)|(\s*$)/gi,"");
+      s = s.replace(/[ ]{2,}/gi," ");
+      s = s.replace(/\n /,"\n");
+      return s.split(' ').length;
+    };
+
+    jQuery.validator.addMethod("lessThan150Words", function(value, element) {
+      return this.optional(element) || (countWords(value) <= 150);
+    }, "Please write less than 150 words.");
+
+    $("#ss-form").validate();
+  }
+});
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -114,24 +114,6 @@ require([
     }
 
     /*
-     * Pebble's good code
-     */
-    if ($.validator) {
-      var countWords = function(s) {
-        s = s.replace(/(^\s*)|(\s*$)/gi,"");
-        s = s.replace(/[ ]{2,}/gi," ");
-        s = s.replace(/\n /,"\n");
-        return s.split(' ').length;
-      };
-
-      jQuery.validator.addMethod("lessThan150Words", function(value, element) {
-        return this.optional(element) || (countWords(value) <= 150);
-      }, "Please write less than 150 words.");
-
-      $("#ss-form").validate();
-    }
-
-    /*
     * Marks archive page JS hacking
     */
     function populateArchive(elemSel) {
