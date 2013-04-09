090896d92ea1eb66425f72645b476b45e4f7fcd7
Tak Tran <contact@tutaktran.com>
2013-04-09 15:10:08 +0100

    Handle invalid url by removing location hash (with html5 goodness for those who support it)

--- public/javascripts/app/people.js
+++ public/javascripts/app/people.js
@@ -10,6 +10,29 @@ require([
     return _s.slugify(name);
   }
 
+  // From http://stackoverflow.com/a/5298684/111884
+  function removeHash() {
+    var scrollV, scrollH, loc = window.location;
+
+    if ("pushState" in history)
+      history.pushState("", document.title, loc.pathname + loc.search);
+    else {
+      // Prevent scrolling by storing the page's current scroll offset
+      scrollV = document.body.scrollTop;
+      scrollH = document.body.scrollLeft;
+
+      loc.hash = "";
+
+      // Restore the scroll offset, should be flicker free
+      document.body.scrollTop = scrollV;
+      document.body.scrollLeft = scrollH;
+    }
+  }
+
+  function havePersonSlug(slug) {
+    return ($("#" + slug).length > 0);
+  }
+
   // Url handling
   var AppRouter = Backbone.Router.extend({
     routes: {
@@ -19,7 +42,11 @@ require([
   var appRouter = new AppRouter();
   appRouter.on('route:getPerson', function(person) {
     var personSlug = slug(person);
-    selectPerson(personSlug);
+    if (havePersonSlug(personSlug)) {
+      selectPerson(personSlug);
+    } else { // No slug available
+      removeHash();
+    }
   });
 
   Backbone.history.start();
