e8a65b215f3593bf410f63b9985133abf071881c
Tak Tran <contact@tutaktran.com>
2013-04-09 16:52:54 +0100

    Remove unnecessary click handler (router handles it already)

--- public/javascripts/app/people.js
+++ public/javascripts/app/people.js
@@ -4,12 +4,4 @@ require([
 ], function($, urlHandler) {
   "use strict";
 
-  // Spotlight changes when person is clicked on
-  $('.person').click(function(event) {
-    var clickTarget = event.target,
-      personLink = $(clickTarget).is("a") ? clickTarget : $(clickTarget).parents("a").first(),
-      personSlug = $(personLink).attr("data-person-slug");
-
-    urlHandler.selectSlug(personSlug);
-  });
 });
