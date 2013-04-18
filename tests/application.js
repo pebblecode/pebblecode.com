c9797d67755093526d96c4ee57ebc4f420b6a2e3
Tak Tran <contact@tutaktran.com>
2013-04-18 12:41:07 +0100

    Clean up homepage test.

--- tests/application.js
+++ tests/application.js
@@ -1,12 +1,8 @@
 /*global casper:false */
+var url = 'http://localhost:7100/';
 
-var BASE_URL = 'http://localhost:7100/';
-
-casper.test.comment('go to homepage');
-
-casper.start(BASE_URL, function() {
-
-  var homepage = BASE_URL;
+casper.test.comment('Homepage');
+casper.start(url, function() {
 
   this.test.assertExists("body.page-homepage");
 
