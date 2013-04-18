a5a70bd3e653bd5d48b3eb270c4c29a7cca810cf
Tak Tran <contact@tutaktran.com>
2013-04-18 16:25:48 +0100

    Rename bloglink to blog_link.

--- models/project.rb
+++ models/project.rb
@@ -7,7 +7,7 @@ class Project
   # :pebblers = who created the project
   # :github = url of the project on github
   # :live_link = link to a live demo of project (if it exsits)
-  # :bloglink = url of blog post relating to project (if applicatble)
+  # :blog_link = url of blog post relating to project (if applicatble)
   # :blogtext = text for blog link (if applicatble)
   # :image = an image for the project (if applicatble)
   @projects = [
@@ -18,7 +18,7 @@ class Project
       :pebblers => "George, Tak, and Mark",
       :type => "product",
       :live_link => "http://tricklrapp.com/",
-      :bloglink => "http://blog.pebblecode.com/post/15777853652/tricklr-1-0",
+      :blog_link => "http://blog.pebblecode.com/post/15777853652/tricklr-1-0",
       :blogtext => "Tricklr 1.0 : blog post",
       :image => "/images/projects/tricklr.jpg"
     },
@@ -30,7 +30,7 @@ class Project
       :type => "ten-time",
       :github => "https://github.com/markdurrant/noisy-uris",
       :live_link => "http://markdurrant.github.com/noisy-uris/",
-      :bloglink => "http://blog.pebblecode.com/post/43139609779/noisy-uris",
+      :blog_link => "http://blog.pebblecode.com/post/43139609779/noisy-uris",
       :blogtext => "Noisy URIs : blog post",
       :image => "/images/projects/noise.jpg"
     },
@@ -41,7 +41,7 @@ class Project
       :pebblers => "Tak and Mark",
       :type => "product",
       :live_link => "http://vistazoapp.com/",
-      :bloglink => "http://blog.pebblecode.com/post/21434355185/vistazo-the-making-of-our-new-baby",
+      :blog_link => "http://blog.pebblecode.com/post/21434355185/vistazo-the-making-of-our-new-baby",
       :blogtext => "The making of our new baby : blog post",
       :image => "/images/projects/vistazo.jpg"
     },
@@ -53,7 +53,7 @@ class Project
       :pebblers => "Tak and Matt",
       :live_link => "http://pebblecode.github.com/wtf/",
       :github => "https://github.com/pebblecode/wtf/",
-      :bloglink => "http://blog.pebblecode.com/post/33705863399/games-hack-day",
+      :blog_link => "http://blog.pebblecode.com/post/33705863399/games-hack-day",
       :blogtext => "part of the pebble games hackday : blog post",
     },
     {
@@ -62,7 +62,7 @@ class Project
       :date => "October 2012",
       :pebblers => "Mark and Vince",
       :type => "games",
-      :bloglink => "http://blog.pebblecode.com/post/33705863399/games-hack-day",
+      :blog_link => "http://blog.pebblecode.com/post/33705863399/games-hack-day",
       :blogtext => "part of the pebble games hackday : blog post",
       :image => "/images/projects/keyboard-demon.jpg"
     },
@@ -72,7 +72,7 @@ class Project
       :date => "October 2012",
       :pebblers => "Joseph, Akash, Sava, and Toby",
       :type => "games",
-      :bloglink => "http://blog.pebblecode.com/post/33705863399/games-hack-day",
+      :blog_link => "http://blog.pebblecode.com/post/33705863399/games-hack-day",
       :blogtext => "part of the pebble games hackday : blog post",
     },
     {
@@ -82,7 +82,7 @@ class Project
       :pebblers => "Daniel, Tom, and Plamen",
       :type => "games",
       :github => "https://github.com/pebblecode/hangover-hunt",
-      :bloglink => "http://blog.pebblecode.com/post/33705863399/games-hack-day",
+      :blog_link => "http://blog.pebblecode.com/post/33705863399/games-hack-day",
       :blogtext => "part of the pebble games hackday : blog post",
     },
     {
@@ -91,7 +91,7 @@ class Project
       :description => "A quiz gameplay engine for <a href='http://campfirenow.com/'>campfire</a>.",
       :pebblers => "Alex and James",
       :type => "games",
-      :bloglink => "http://blog.pebblecode.com/post/33705863399/games-hack-day",
+      :blog_link => "http://blog.pebblecode.com/post/33705863399/games-hack-day",
       :blogtext => "part of the pebble games hackday : blog post",
     },
     {
@@ -102,7 +102,7 @@ class Project
       :type => "ten-time",
       :live_link => "http://pebblecode.github.com/PCR/",
       :github => "https://github.com/pebblecode/PCR",
-      :bloglink => "http://blog.pebblecode.com/post/29614270562/the-great-css-experiment",
+      :blog_link => "http://blog.pebblecode.com/post/29614270562/the-great-css-experiment",
       :blogtext => "pebble custom reset blog post : blog post",
     },
     {
@@ -112,7 +112,7 @@ class Project
       :pebblers => "Alex and Tak",
       :type => "olympic",
       :live_link => "http://pebblecode.com/olympic-data-vis",
-      :bloglink => "http://blog.pebblecode.com/post/27976232997/olympics-hackery",
+      :blog_link => "http://blog.pebblecode.com/post/27976232997/olympics-hackery",
       :blogtext => "Olympics hack day : blog post",
     },
     {
@@ -123,7 +123,7 @@ class Project
       :type => "olympic",
       :github => "https://github.com/pebblecode/fear-the-IOC/",
       :live_link => "http://pebblecode.github.com/fear-the-IOC/",
-      :bloglink => "http://blog.pebblecode.com/post/21838814065/hack-day-live",
+      :blog_link => "http://blog.pebblecode.com/post/21838814065/hack-day-live",
       :blogtext => "part of the pebble olympics hackday : blog post",
       :image => "/images/projects/fear.jpg"
     }
