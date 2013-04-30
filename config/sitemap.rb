e4cfbae60a339641a09ca6dbd7847211deccd5ee
Tak Tran <contact@tutaktran.com>
2013-04-30 15:21:38 +0100

    Remove /contact_us page from sitemap.

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -4,7 +4,6 @@ require 'sitemap_generator'
 SitemapGenerator::Sitemap.default_host = 'http://pebblecode.com'
 SitemapGenerator::Sitemap.create do
   add '/', :changefreq => 'daily', :priority => 0.9
-  add '/contact_us', :changefreq => 'weekly'
   add '/people', :changefreq => 'weekly'
   add '/work', :changefreq => 'weekly'
   add '/labs', :changefreq => 'weekly'
