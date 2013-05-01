7b5ee1e271b209479407d84b5190c4d2dc9b7eb0
Tak Tran <contact@tutaktran.com>
2013-05-01 17:49:56 +0100

    Create people backbone collection.

--- public/javascripts/app/people.js
+++ public/javascripts/app/people.js
@@ -1,6 +1,16 @@
+/*global window:false */
 define([
+  "backbone",
   "urlHandler"
-], function(urlHandler) {
+], function(Backbone, urlHandler) {
   "use strict";
 
+  var Person = Backbone.Model.extend(),
+    People = Backbone.Collection.extend({
+      model: Person
+    }),
+    people = new People(window.PEOPLE); // Init from global
+
+
+  window.people = people;
 });
