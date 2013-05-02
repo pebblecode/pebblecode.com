35684a34c482d5b60e9c264584169c1d535f5766
Tak Tran <contact@tutaktran.com>
2013-05-02 10:20:31 +0100

    Use a backbone view to display model.

--- public/javascripts/app/people.js
+++ public/javascripts/app/people.js
@@ -12,22 +12,40 @@ define([
       model: Person
     }),
     people = new People(window.PEOPLE), // Init from global
-    personTemplate = _.template($("#person-template").html());
+    PersonView = Backbone.View.extend({
+      template: _.template($("#person-template").html()),
+      initialize: function(options){
+        this.model = options.model;
+        this.listenTo(this.model, 'change', this.render);
+      },
+      destroy: function(){
+        delete this.model;
+      },
+      render: function() {
+        var person = this.model;
+
+        this.$el.html(this.template({
+          person: person,
+          id: _s.slugify(person.get('name')),
+          name: person.get('name'),
+          title: person.get('title'),
+          tumblr_name: person.get('tumblr_name'),
+          big_image: "/images/mug-shots/big/" + person.get('image'),
+          description: person.get('description'), // TODO: Use markdown
+          website: person.get('website'),
+          twitter: person.get('twitter'),
+          github: person.get('github'),
+          linkedin: person.get('linkedin')
+        }));
+        return this;
+      }
+    }),
+    personView = new PersonView({
+      el: $("#spotlight"),
+      model: people.first()
+    });
 
   // TODO: Test code
   window.people = people;
-  var person = people.first();
-  window.personHtml = personTemplate({
-    person: person,
-    id: _s.slugify(person.get('name')),
-    name: person.get('name'),
-    title: person.get('title'),
-    tumblr_name: person.get('tumblr_name'),
-    big_image: "/images/mug-shots/big/" + person.get('image'),
-    description: person.get('description'), // TODO: Use markdown
-    website: person.get('website'),
-    twitter: person.get('twitter'),
-    github: person.get('github'),
-    linkedin: person.get('linkedin')
-  });
+  window.personView = personView;
 });
