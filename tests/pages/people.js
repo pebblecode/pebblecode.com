b1d39e05d210b2ea143c26c11dd1a9933f251983
Tak Tran <contact@tutaktran.com>
2013-05-07 16:56:49 +0100

    Don't need to echo img tag in test.

--- tests/pages/people.js
+++ tests/pages/people.js
@@ -8,7 +8,7 @@ casper.start(prodPaths("people"))
     this.click(".person[href='/people/toby-hunt']");
     this.test.assertUrlMatch(/\/people\/toby-hunt/, "Changes url");
     this.test.assertVisible("#spotlight-scroll", "Scrolls to #spotlight-scroll");
-    this.test.assertSelectorExists(".person-row img[src='/images/mug-shots/big/toby.png']", "Changes image (" + casper.echo(this.getHTML('.person-row img', true)) + ")");
+    this.test.assertSelectorExists(".person-row img[src='/images/mug-shots/big/toby.png']", "Changes image (" + this.getHTML('.person-row img', true) + ")");
   });
 
 casper.run(function() {
