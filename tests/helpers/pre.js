e20dac56b4e047d8dc26496cf630a7c75163c5d4
Tak Tran <contact@tutaktran.com>
2013-05-07 16:35:55 +0100

    Update people integration test.

--- tests/helpers/pre.js
+++ tests/helpers/pre.js
@@ -3,13 +3,23 @@
 casper.test.comment('Load pre.js');
 
 var utils = {
-  baseUrl: "http://localhost:7100/"
+  baseUrl: "http://192.168.3.148:7888/"
 };
 
 var paths = {
   homepage: utils.baseUrl,
   labs: utils.baseUrl + "labs",
   people: utils.baseUrl + "people"
+};
+
+/**
+ * Production paths
+ *
+ * @param  {String} name Name of the url for `paths` key
+ * @return {String} Production path
+ */
+var prodPaths = function(name) {
+  return paths[name] + "?prod";
 };
 
 casper.test.done();
--- tests/pages/people.js
+++ tests/pages/people.js
@@ -1,12 +1,15 @@
 /*global casper:false */
 
 casper.test.comment('People');
-casper.start(paths.people, function() {
+casper.start(prodPaths("people"))
+  .then(function() {
+    this.test.comment("Clicking on person link");
 
-  // Has spotlight scroll element
-  this.test.assertExists("#spotlight-scroll");
-
-});
+    this.click(".person[href='/people/toby-hunt']");
+    this.test.assertUrlMatch(/\/people\/toby-hunt/, "Changes url");
+    this.test.assertVisible("#spotlight-scroll", "Scrolls to #spotlight-scroll");
+    this.test.assertSelectorExists(".person-row img[src='/images/mug-shots/big/toby.png']", "Changes image (" + casper.echo(this.getHTML('.person-row img', true)) + ")");
+  });
 
 casper.run(function() {
   this.test.done();
