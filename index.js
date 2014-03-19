ac7eab2c9855b0b9d2fa689bcb1cdf526caffcfd
Tak Tran <contact@tutaktran.com>
2014-03-20 10:35:35 +1100

    [#111] Remove .html extension when responding to page requests.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var logfmt = require("logfmt");
 var app = express();
+var distFolder = __dirname + '/dist';
 
 var port = process.env.PORT || 8080;
 var env = process.env.ENV || "development";
@@ -18,11 +19,26 @@ if (env === "staging") {
 
 app.use(logfmt.requestLogger());
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(distFolder));
 app.use(express.errorHandler({
   dumpExceptions: true,
   showStack: true
 }));
+app.engine('html', require('ejs').renderFile);
+
+// Simple processing of pages
+// Mainly to remove .html extension
+app.get('/:page', function(req, res) {
+  var page = req.param('page');
+
+  if (page) {
+    var pageFile = page + '.html';
+    var path = distFolder + "/" + pageFile;
+    res.render(path);
+  } else {
+    throw "Can't find page: " + page;
+  }
+});
 
 var port = Number(port);
 app.listen(port, function() {
