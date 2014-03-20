30055c13799c5edefb39a9facf7d56aa2e635651
Tak Tran <contact@tutaktran.com>
2014-03-20 16:17:27 +1100

    [#110] Add deployment to production grunt task.

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,10 +3,20 @@ module.exports = function ( grunt ) {
   // use strick mode for JS
   'use strict';
 
+  var shell = require('shelljs');
+
+  var echoCmd = function(cmd) {
+    return "echo '" + cmd + "'";
+  };
+
   // grunt config options
   grunt.initConfig({
     // read package.json
     pkg: grunt.file.readJSON( 'package.json' ),
+
+    constants: {
+      tempProdFolder: "temp/production"
+    },
 
     // assemble (static site generator) options
     assemble: {
@@ -176,9 +186,17 @@ module.exports = function ( grunt ) {
         },
         command: function() {
           var stagingInit = "git remote add staging git@heroku.com:pebblecode-staging.git";
+          var tempProdFolder = grunt.config.get('constants').tempProdFolder;
+          var productionInit = "mkdir temp;" +
+            "git clone git@github.com:pebblecode/pebblecode.github.io.git " + tempProdFolder;
+          var commands = [
+            echoCmd(stagingInit),
+            stagingInit,
+            echoCmd(productionInit),
+            productionInit
+          ];
 
-          return "echo '" + stagingInit + "';" +
-                 stagingInit;
+          return commands.join('; ');
         }
       },
       deployStaging: {
@@ -201,10 +219,29 @@ module.exports = function ( grunt ) {
             stagingDeployCmd = stagingDeployCmd + " -f";
           }
 
-          return "echo '" + stagingDeployCmd + "';" +
-                 stagingDeployCmd;
+          var commands = [
+            echoCmd(stagingDeployCmd),
+            stagingDeployCmd
+          ];
+
+          return commands.join('; ');
         }
       }
+    },
+
+    prompt: {
+      commitMessage: {
+        options: {
+          questions: [
+            {
+              config: 'commitMessage',
+              type: 'input',
+              message: 'Please enter a commit message for the new production files:',
+              default: ''
+            }
+          ]
+        }
+      },
     },
 
     'gh-pages': {
@@ -227,6 +264,7 @@ module.exports = function ( grunt ) {
   grunt.loadNpmTasks( 'grunt-gh-pages' );
   grunt.loadNpmTasks( 'grunt-contrib-uglify' );
   grunt.loadNpmTasks( 'grunt-shell' );
+  grunt.loadNpmTasks( 'grunt-prompt' );
 
   grunt.registerTask( 'default', [ 'make', 'connect', 'watch' ] );
 
@@ -234,4 +272,51 @@ module.exports = function ( grunt ) {
 
   grunt.registerTask( 'deploy:init', ['shell:init'] );
   grunt.registerTask( 'deploy:staging', ['shell:deployStaging'] );
+  grunt.registerTask( 'deploy:production', function() {
+    var tempProdFolder = grunt.config.get('constants').tempProdFolder;
+    var gitStatusCmd = 'git status -s';
+    var status = shell.exec(gitStatusCmd, { silent:true });
+    var msg;
+
+    // Check that deployment init has been run
+    if (!grunt.file.exists(tempProdFolder)) {
+      throw new Error("Deployment not initialised yet. Run `grunt deploy:init` first.");
+    }
+
+    // Check that there aren't any uncommited changes
+    if (status.output !== "") {
+      msg = 'You have uncommited changes:\n\n' + status.output + "\n" +
+        "Commit or stash changes before proceeding.\n\n";
+      throw new Error(msg);
+    }
+
+    // Remove old files
+    shell.rm('-r', tempProdFolder + '/*');
+
+    // Copy files across
+    shell.cp('-R', 'dist/*', tempProdFolder);
+
+    // Check for changes, and run commit task if
+    // there are
+    shell.cd(tempProdFolder);
+    var prodStatus = shell.exec(gitStatusCmd, { silent: true });
+    if (prodStatus.output === "") {
+      console.log("Nothing changed.");
+    } else {
+      grunt.log.writeln("The following files changed: \n" + prodStatus.output);
+      grunt.task.run([
+        'prompt:commitMessage',
+        'deploy:production:pushChanges'
+      ]);
+    }
+  });
+
+  // Push changes from temp folder to production
+  grunt.registerTask('deploy:production:pushChanges', function() {
+    // From grunt prompt
+    var commitMessage = grunt.config('commitMessage');
+    shell.cd(tempProdFolder);
+    shell.exec('git commit -m "' + commitMessage + '"');
+    shell.exec('git push');
+  });
 };
--- README.md
+++ README.md
@@ -71,9 +71,13 @@ It was initialised with
 
 The production site is hosted on github pages: http://pebblecode.com/, with files hosted at https://github.com/pebblecode/pebblecode.github.io
 
-Currently deployment is manual, with the following process:
+The deploy to production, run
 
-1. Development this [repo](https://github.com/pebblecode/pebblecode.com)
-2. Files are auto-generated in `dist` during development
-3. Copy files to https://github.com/pebblecode/pebblecode.github.io repo, and push to `master`
+    grunt deploy:production
+
+The grunt task does the following:
+
+1. Copies files from `dist` to `temp/production`
+2. Copy files to https://github.com/pebblecode/pebblecode.github.io repo
+3. Pushes to `master` branch
 
