e59fbea5d5200423488a2f20c15026119cffef81
Peter Tait <contact@petertait.com>
2014-08-12 12:19:19 +0100

    Removal of Map Carousel
    Couldn’t work out how to do vertical carousel, when height is dynamic.

--- src/js/main.js
+++ src/js/main.js
@@ -88,5 +88,121 @@ $(document).ready(function () {
       });
     }
   });
+
+  // contact page
+  // Map Carousel - first checks if .map-carousel exists then creates carousel
+  if ($('.map-carousel').length > 0) {
+    $(".map-carousel").tinycarousel({
+        axis   : "x",
+        start: 0,
+        animationTime: 500
+        // callback: function(e){
+        //   $('.viewport, .overview li').css({'height': ($(e).height() + 10) +'px'});
+        // }
+      });
+    var mapCarousel = $(".map-carousel").data("plugin_tinycarousel");
+    $('.map-carousel-nav a').click(function(){
+      $('.map-carousel-nav a').removeClass('active');
+      $(this).addClass('active');
+    });
+      $('.map-carousel .london').click(function(){
+          mapCarousel.move(0);
+          return false;
+      });
+      $('.map-carousel .edinburgh').click(function(){
+          mapCarousel.move(1);
+          return false;
+      });
+      $('.map-carousel .newcastle').click(function(){
+          mapCarousel.move(3);
+          return false;
+      });
+      $('.map-carousel .sofia').click(function(){
+          mapCarousel.move(4);
+          return false;
+      });
+
+      // var oSlider = $('.map-carousel');
+      // oSlider.tinycarousel({
+      // callback: function(element){
+      //     element.css({ 'height': 100 });
+      //     $('.viewport', oSlider).css({ 'height': 100});
+      //   }
+      // });
+  }
   
 });
+
+// Google Maps
+window.onload = function() {
+
+  
+
+  // contact page
+  // Checks if Map Carousel exists, then gets google maps
+  if ($('.map-carousel').length > 0) {
+
+    $('.site-footer').hide();
+    
+    // Google map: London
+    var mapLondon = {
+      center: new google.maps.LatLng(51.485672, -0.118554),
+      zoom: 15,
+      scrollwheel: false,
+      disableDefaultUI: true
+    };
+    var map = new google.maps.Map(document.getElementById("london-map"),
+      mapLondon);
+    var marker = new google.maps.Marker({
+      position: new google.maps.LatLng(51.485672, -0.118554),
+      map: map,
+      title: "pebble {code}"
+    });
+
+    // Google map: Edinburgh
+    var mapEdinburgh = {
+      center: new google.maps.LatLng(55.8959774, -3.296969),
+      zoom: 15,
+      scrollwheel: false,
+      disableDefaultUI: true
+    };
+    var map = new google.maps.Map(document.getElementById("edinburgh-map"),
+      mapEdinburgh);
+    var marker = new google.maps.Marker({
+      position: new google.maps.LatLng(55.8959774, -3.296969),
+      map: map,
+      title: "pebble {code}"
+    });
+
+    // Google map: Newcastle
+    var mapNewcastle = {
+      center: new google.maps.LatLng(55.0016746, -1.6156206),
+      zoom: 15,
+      scrollwheel: false,
+      disableDefaultUI: true
+    };
+    var map = new google.maps.Map(document.getElementById("newcastle-map"),
+      mapNewcastle);
+    var marker = new google.maps.Marker({
+      position: new google.maps.LatLng(55.0016746, -1.6156206),
+      map: map,
+      title: "pebble {code}"
+    });
+
+    // Google map: Sofia
+    var mapSofia = {
+      center: new google.maps.LatLng(42.6742392, 23.3543577),
+      zoom: 15,
+      scrollwheel: false,
+      disableDefaultUI: true
+    };
+    var map = new google.maps.Map(document.getElementById("sofia-map"),
+      mapSofia);
+    var marker = new google.maps.Marker({
+      position: new google.maps.LatLng(42.6742392, 23.3543577),
+      map: map,
+      title: "pebble {code}"
+    });
+  }
+
+};
