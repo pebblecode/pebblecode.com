e19615fd1662824f2514861fd32abb657c505c7d
Peter Tait <contact@petertait.com>
2015-05-05 17:28:10 +0100

    initial services page

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,15 @@
 (function () { 'use strict';
 
+  // Mobile nav
+  function navMenu() {
+    document.getElementById('navMenu').classList.toggle('active');
+    this.classList.toggle('active');
+  }
+  document.getElementById('navBtn').addEventListener('click', navMenu);
+
+  // Tabs
+  function tabs() {
+
+  }
+
 }()); // end 'use strict'
