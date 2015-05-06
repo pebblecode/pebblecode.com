99a431a40d91fc8076918cd47c3dcc0746575693
Peter Tait <contact@petertait.com>
2015-05-06 15:58:37 +0100

    added people, style tweaks

--- src/js/main.js
+++ src/js/main.js
@@ -1,57 +1,110 @@
-(function () { 'use strict';
+(function () {'use strict';
 
   // Mobile nav
-  var gblHeadBtn = $( '.gbl-head-btn');
-  var gblHeadNav = $( '.gbl-head-nav');
+  var gblHeadBtn = $('.gbl-head-btn');
+  var gblHeadNav = $('.gbl-head-nav');
   gblHeadBtn.click( function() {
-    $(this).toggleClass( 'active' );
-    gblHeadNav.toggleClass( 'active' );
+    $(this).toggleClass('active');
+    gblHeadNav.toggleClass('active');
   });
 
   // Tabs
-  var tabHeadItem = $( '.tab-head-item' );
-  var tabContent = $( '.tab-content' );
+  var tabHeadItem = $('.tab-head-item');
+  var tabContent = $('.tab-content');
   tabHeadItem.click( function() {
     $(this).siblings().removeClass('active');
     $(this).addClass('active');
     var tabID = $(this).data('tab-id');
-    $('#' + tabID).siblings( '.tab-content' ).removeClass('active');
+    $('#' + tabID).siblings('.tab-content').removeClass('active');
     $('#' + tabID).addClass('active');
   });
 
   // Change services bg colour on scroll
+  var servicesContainer = $('.services-container');
   function servicesBG() {
     var windowTop = $(window).scrollTop();
-    var servicesStrategy = $( '#servicesStrategy' ).offset().top - 250;
-    var servicesHacking = $( '#servicesHacking' ).offset().top - 250;
-    var servicesUX = $( '#servicesUX' ).offset().top - 250;
-    var servicesUXdesign = $( '#servicesUXdesign' ).offset().top - 250;
-    var servicesDesign = $( '#servicesDesign' ).offset().top - 250;
-    var servicesAgile = $( '#servicesAgile' ).offset().top - 250;
-    var servicesTests = $( '#servicesTests' ).offset().top - 250;
-    var servicesCollab = $( '#servicesCollab' ).offset().top - 250;
-
+    var servicesStrategy = $('#servicesStrategy').offset().top - 200;
+    var servicesHacking = $('#servicesHacking').offset().top - 200;
+    var servicesUX = $('#servicesUX').offset().top - 200;
+    var servicesUXdesign = $('#servicesUXdesign').offset().top - 200;
+    var servicesDesign = $('#servicesDesign').offset().top - 200;
+    var servicesAgile = $('#servicesAgile').offset().top - 200;
+    var servicesTests = $('#servicesTests').offset().top - 200;
+    var servicesCollab = $('#servicesCollab').offset().top - 200;
     if ( windowTop > servicesStrategy  ){
-      $( '.services-container' ).css({ 'background-color' : '#9b5ca4' });
+      servicesContainer.css({'background-color':'#9b5ca4' });
     } if ( windowTop > servicesHacking ) {
-      $( '.services-container' ).css({ 'background-color' : '#ed4f7e' });
+      servicesContainer.css({'background-color':'#ed4f7e' });
     } if ( windowTop > servicesUX ) {
-      $( '.services-container' ).css({ 'background-color' : '#0ea2dc' });
+      servicesContainer.css({'background-color':'#0ea2dc' });
     } if ( windowTop > servicesUXdesign ) {
-      $( '.services-container' ).css({ 'background-color' : '#37bec0' });
+      servicesContainer.css({'background-color':'#37bec0' });
     } if ( windowTop > servicesDesign ) {
-      $( '.services-container' ).css({ 'background-color' : '#a4ce4e' });
+      servicesContainer.css({'background-color':'#a4ce4e' });
     } if ( windowTop > servicesAgile ) {
-      $( '.services-container' ).css({ 'background-color' : '#faad40' });
+      servicesContainer.css({'background-color':'#faad40' });
     } if ( windowTop > servicesTests ) {
-      $( '.services-container' ).css({ 'background-color' : '#9b5ca4' });
+      servicesContainer.css({'background-color':'#9b5ca4' });
     } if ( windowTop > servicesCollab ) {
-      $( '.services-container' ).css({ 'background-color' : '#ed4f7e' });
+      servicesContainer.css({'background-color':'#ed4f7e' });
     }
   }
-  $(function() {
-    $(window).scroll(servicesBG);
-    servicesBG();
+  if ($( servicesContainer ).length > 0) { 
+    $(function() {
+      $(window).scroll(servicesBG);
+      servicesBG();
+    });
+  }
+
+  // Google Maps: Pan between different Locations
+  $('#londonBtn').click( function() {
+    changeMarkerPos(51.485672, -0.118554);
   });
+  $('#edinburghBtn').click( function() {
+    changeMarkerPos(55.8959774, -3.296969);
+  });
+  $('#newcastleBtn').click(function(){
+    changeMarkerPos(55.0016746, -1.6156206);
+  });
+  $('#sofiaBtn').click(function(){
+    changeMarkerPos(42.6742392, 23.3543577);
+  });
+
+  var marker;
+  var map;
+  function initialize() {
+    var mapProp = {
+      center: new google.maps.LatLng(51.485672, -0.118554),
+      zoom: 15,
+      panControl: false,
+      zoomControl: false,
+      mapTypeControl: false,
+      scaleControl: true,
+      streetViewControl: false,
+      overviewMapControl: false,
+      rotateControl: true,
+      scrollwheel: false,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    map = new google.maps.Map(document.getElementById("map"), mapProp);
+    marker = new google.maps.Marker({
+      position: new google.maps.LatLng(51.485672, -0.118554),
+      animation: google.maps.Animation.DROP
+    });
+    marker.setMap(map);
+    map.panTo(marker.position);
+    google.maps.event.addListener(marker, "click", function () {
+    });
+  }
+
+  function changeMarkerPos(lat, lon){
+    myLatLng = new google.maps.LatLng(lat, lon);
+    marker.setPosition(myLatLng);
+    map.panTo(myLatLng);
+  }
+
+  if ($('#map').length) {
+    google.maps.event.addDomListener(window, 'load', initialize);
+  }
 
 }()); // end 'use strict'
